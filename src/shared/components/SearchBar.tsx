8e2a03f231b5f831f03d96db7bb7d18dc0cc9c7d
diana <dianaclabel08@gmail.com>
2026-03-18 21:47:18 -0500

    feat: add useEffect to SearchBar for debounced query handling

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { useState, type KeyboardEvent } from "react";
+import { useEffect, useState, type KeyboardEvent } from "react";
 
 interface Props {
   placeholder?: string,
@@ -8,12 +8,37 @@ interface Props {
 export const SearchBar = ({ placeholder = "Buscar", onQuery }: Props) => {
   //Se usa useState dentro de este componente porque este sera un estado
   // que solo le importa a searhbar
+  //useState
   const [query, setQuery] = useState('');
 
+  //UseEffect
+  //Se dispara despue de que renderice el componente
+
+  //dependencias[]: Su función principal es decirle a React cuándo debe volver a ejecutarse 
+  // el código que escribiste dentro del efecto. Si no lo pusieras, el efecto se ejecutaría en cada renderizado del componente, lo cual suele ser ineficiente o causar bucles infinitos.
+
+  useEffect(() => {
+    const timeoutid = setTimeout(() => {
+      onQuery(query)
+    }, 700)
+    //---------
+    //onQuery(query);
+    //funcion de limpieza se ejecuta cuando el componente deja de existir(desmonta) o hay una actualizacion del componenete (re-render)
+    return () => {
+      //ejm de cuando un comp deja de existir -> { false && (< SearchBar placeholder="Busca lo que quieras" onQuery={handleSearch}>)}
+      // como no se renderiza entonces se ejecuta el return
+      // console.log('función de limpieza')
+      //-------
+      clearTimeout(timeoutid);
+    }
+
+  }, [query, onQuery]);//dependencias
+
+  //handles
   const handleSearch = () => {
-    onQuery(query)
+    //onQuery(query)
     //esto me permite limpiar el setQuery, cuando se ejecute se borrara lo escrito en el input
-    setQuery("")
+    // setQuery("")
   }
 
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
