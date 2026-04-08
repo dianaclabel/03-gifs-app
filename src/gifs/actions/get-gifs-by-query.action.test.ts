44096e4637008ae2458aee6087e3de38dbe1af36
Diana clabel <dianaclabel08@gmail.com>
2026-04-07 23:59:15 -0500

    Refactor code structure for improved readability and maintainability

--- src/gifs/actions/get-gifs-by-query.action.test.ts
+++ src/gifs/actions/get-gifs-by-query.action.test.ts
@@ -1,25 +1,52 @@
 import { describe, expect, test } from "vitest";
+import AxiosMockAdapter from 'axios-mock-adapter';
+
 import { getGifsByQuery } from "./get-gifs-by-query.action";
+import { giphyApi } from "../api/giphy.api";
+
+import { giphySearchResponseMock } from '../../../test/mock/giphy.response.data';
 
 describe('getGifsByQuery',()=>{
+
+  const  mock = new AxiosMockAdapter(giphyApi);
+
   test('should return a list of gifs', async ()=>{
-     const gifs = await getGifsByQuery('Goku');
-     //Se hace la desestructuracion
-     const [ gif1 ] = gifs
+    //  const gifs = await getGifsByQuery('Goku');
+    //  //Se hace la desestructuracion
+    //  const [ gif1 ] = gifs
 
-    expect(gifs.length).toBe(10);
+    // expect(gifs.length).toBe(10);
 
-    expect(gif1).toStrictEqual({
-          id: expect.any(String),
-          height: expect.any(Number),
-          width: expect.any(Number),
-          title:expect.any(String),
-          url:expect.any(String),
-    });
+    // expect(gif1).toStrictEqual({
+    //       id: expect.any(String),
+    //       height: expect.any(Number),
+    //       width: expect.any(Number),
+    //       title:expect.any(String),
+    //       url:expect.any(String),
+    // });
     
 
     //  expect(gifs).toStrictEqual(gifs)
      
+  })
+
+  test('should return a lift of gifs', async()=>{
+    
+    mock.onGet('/search').reply(200, giphySearchResponseMock)
+
+    const gifs =  await getGifsByQuery('goku');
+    console.log(gifs);
+
+    expect(gifs.length).toBe(10);
+
+    gifs.forEach( gif => {
+      expect ( typeof gif.id).toBe('string');
+      expect ( typeof gif.title).toBe('string');
+      expect ( typeof gif.url).toBe('string');
+      expect ( typeof gif.width).toBe('number');
+      expect ( typeof gif.height).toBe('number');
+    })
+    
   })
 }
 )
